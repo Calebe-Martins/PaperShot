7ec2023a93b76acab05068808a95ad4d36ac15b2
Martins <calebe.graciano@gmail.com>
2021-11-23 09:35:51 -0300

    MovimentoCamera
    Camera olha pros lados e pra cima e pra baixo

---
+++ PaperShotProject/Assets/Player/Script/MovimentoCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCamera : MonoBehaviour
+{
+    [Header("Configurações Mouse")]
+    public float sensibilidadeMouse = 100f;
+    public float anguloMin = -90;
+    public float anguloMax = 90;
+    
+    public Transform transformPlayer;
+
+    float rotacao = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidadeMouse * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensibilidadeMouse * Time.deltaTime;
+
+        rotacao -= mouseY;
+        rotacao = Mathf.Clamp(rotacao, anguloMin, anguloMax);
+
+        transform.localRotation = Quaternion.Euler(rotacao, 0, 0);
+
+        transformPlayer.Rotate(Vector3.up * mouseX);
+    }
+}
